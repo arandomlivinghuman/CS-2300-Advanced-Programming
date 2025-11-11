59a1f75e8850f63b803f65161b6944d384ec564f
GavinMonteiro <gtmontei@uvm.edu>
2025-11-11 13:41:58 -0500

    Update sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,16 +1,9 @@
 public class sum_even_fibonacci {
-    // comment
+    // broke file to return only 0, Gavin Monteiro.
+    
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
-        while (b < 4000000) {
-            if (b % 2 == 0) {
-                sumEven += b;
-            }
-            int temp = b;
-            b = a + b;
-            a = temp;
-        }
         System.out.println(sumEven);
     }
 }
