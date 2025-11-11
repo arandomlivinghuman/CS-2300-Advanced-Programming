aac424ba509be4d89fa9a2ed6b9f1838d836743f
arandomlivinghuman <zbushey@uvm.edu>
2025-11-11 13:43:22 -0500

    Update sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,16 +1,20 @@
+//zb
+//while loop < to >
+//incorrect if statement
+//a, b
 #include <iostream>
 
 int main()
 {
     int a = 0, b = 1;
     int sum_even = 0;
-    while (b < 4000000) {
-        if (b % 2 == 0) {
+    while (b > 4000000) {
+        if (b % 2 != 0) {
             sum_even += b;
         }
         int temp = b;
-        b = a + b;
-        a = temp;
+        a = a + b;
+        b = temp;
     }
     std::cout << sum_even << std::endl;
 
